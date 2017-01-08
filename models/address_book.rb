77d46eb8dd50a5d4e792e3d5032cd5a0939d764e
Praveen <praveenyr@gmail.com>
2017-01-08 15:12:44 +0000

    Complete checkpoint-7-addressbloc-readingcsv

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
  require_relative 'entry'
+ require "csv"
  
  class AddressBook
      attr_accessor :entries
@@ -15,6 +16,16 @@
         index+=1
         end
        entries.insert(index,Entry.new(name,phone_number,email))
+     end
+     
+     def import_from_csv(file_name)
+       csv_text = File.read(file_name)
+       csv = CSV.parse(csv_text,headers: true,skip_blanks: true) #Returns a CSV::Table Object
+       
+       csv.each do |row|
+        row_hash = row.to_hash #Converts each row to a hash
+        add_entry(row_hash["name"],row_hash["phone_number"],row_hash["email"])
+       end
      end
      
    
